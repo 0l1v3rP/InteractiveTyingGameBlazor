175ded3b232dc46487d7eea14d7cbdf05807234a
0L1v3rP <paterekoliver@gmail.com>
2024-02-17 11:45:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+using InteractiveTyingGameBlazor.Components;
+using InteractiveTyingGameBlazor.Components.Account;
+using InteractiveTyingGameBlazor.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddDefaultPolicy(policy =>
+//    {
+//        policy.AllowAnyOrigin()
+//            .AllowAnyHeader()
+//            .AllowAnyMethod();
+//    });
+//});
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+//app.UseRouting();
+//app.UseCors();
+
+//app.UseAuthorization();
+//app.UseStaticFiles(new StaticFileOptions
+//{
+//    OnPrepareResponse = ctx =>
+//    {
+//        ctx.Context.Response.Headers.Append("Access-Control-Allow-Origin", "*");
+//    }
+//});
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+app.Run();
---
+++ wwwroot/js/player.js
@@ -0,0 +1,44 @@
+﻿function initVideoPlayer(videoId, dotNetRef) {
+    const video = document.getElementById(videoId);
+    video.addEventListener('timeupdate', () =>  handleTimeUpdate(dotNetRef, video));
+    video.addEventListener('ended', () => handleVideoEnd(dotNetRef));
+}
+
+ function handleTimeUpdate(dotNetRef, video) {
+    dotNetRef.invokeMethodAsync('updateSubtitles', video.currentTime)
+    return;
+}
+
+function handleVideoEnd(dotNetRef) {
+    
+}
+
+var input = document.getElementById('typingInput');
+function setCursorToStart() {
+    input.setSelectionRange(0, 0);
+}
+
+input.addEventListener('keypress', function (event) {
+    if (event.key === input.value.charAt(0)) {
+        input.value = input.value.substring(1);
+        setCursorToStart();
+    }
+    event.preventDefault();
+});
+
+input.addEventListener('keydown', function (event) {
+    if (event.key === 'Backspace' || ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.key)) {
+        setCursorToStart();
+        event.preventDefault();
+    }
+});
+
+input.addEventListener('click', function (event) {
+    setCursorToStart();
+    event.preventDefault();
+});
+
+function setSubtitle(subtitle) {
+    const typedText = subtitle.lines.join(' ').replace(/…/g, '...');
+    document.getElementById('typingInput').value = typedText;
+}
